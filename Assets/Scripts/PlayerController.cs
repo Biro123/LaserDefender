fe5918ab5e37dc8347b3fb3958518860d886da05
b <biro123@ntlworld.com>
2017-09-02 19:21:30 +0100

    PC04 - restricted movement of player to stay within camera boundaries. Created a spawner which creates a single enemy on startup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed = 5.0f;
+    public float padding = 1.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    float xMin;
+    float xMax;
+
+    // Called once at the start of the scene
+	void Start()
+    {
+        /// find left and rightmost positions of the visible playarea 
+        float distance = this.transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xMin = leftmost.x + padding;
+        xMax = rightmost.x - padding;
+    }
+
 	// Update is called once per frame
 	void Update () {
 
         /// multiplying by Time.deltaTime makes it frame-rate independent, and also 
         /// reduces the speed as it is less than 1. Deltatime = the time difference between
         /// this frame and the last.
+        /// Also note that using an 'if' before moving to constrain to the screen is more 
+        /// efficient than using a clamp to reset it afterwards.
         if( Input.GetKey(KeyCode.LeftArrow) )
         {
-            this.transform.position -= new Vector3(moveSpeed*Time.deltaTime, 0.0f, 0.0f);
+            if (this.transform.position.x >= xMin)
+            {
+                this.transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            }
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.position += new Vector3(moveSpeed*Time.deltaTime, 0.0f, 0.0f);
+            if (this.transform.position.x <= xMax)
+            {
+                this.transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            }
         }
 
     }
