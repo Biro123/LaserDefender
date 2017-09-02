20bbb88c8b682c51eff96be421ec400d0f2fd806
b <biro123@ntlworld.com>
2017-09-02 16:19:26 +0100

    PC03 - Added moveable player spaceship

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /// multiplying by Time.deltaTime makes it frame-rate independent, and also 
+        /// reduces the speed as it is less than 1. Deltatime = the time difference between
+        /// this frame and the last.
+        if( Input.GetKey(KeyCode.LeftArrow) )
+        {
+            this.transform.position -= new Vector3(moveSpeed*Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(moveSpeed*Time.deltaTime, 0.0f, 0.0f);
+        }
+
+    }
+}
