24140d3a6395eff12549f2e64eaa9f943f2bf928
b <biro123@ntlworld.com>
2017-09-03 01:49:35 +0100

    PC06 - Create 'Positions' within the enemy formation using OnDrawGizmos() to display them Spawn enemy at every position as a child to that position. Also re-organised the assets folders

---
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        /// for every transform child in my-transform (ie every Position in EnemyFormation)
+        foreach( Transform child in transform)
+        {
+            /// Spawn an enemy at the position of the child (ie, Position object)
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            /// Move it under the child (ie Position) of this object(EnemyFormation) in the hierarch 
+            enemy.transform.parent = child;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Entities/EnemyFormation/Position.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour {
+
+
+    void OnDrawGizmos()
+    {
+        /// makes the position object visible in the scene as a circle
+        Gizmos.DrawWireSphere(this.transform.position, 1);
+    }
+}
